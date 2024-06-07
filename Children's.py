e392baffeae4ed335760a2d99838fa74b60fa29d
Kersedoo <160669809+Kersedoo@users.noreply.github.com>
2024-06-07 13:35:03 +0100

    Create Children's.py

---
+++ Children's.py
@@ -0,0 +1,14 @@
+class ChildrensAccount(Account):
+    def __init__(self, account_number, your_name, balance=0.0):
+        super().__init__(account_number, your_name, balance)
+        self.interest_rate = 0.007
+    
+    def deposit(self, amount):
+        self._balance += amount
+        self._balance += amount * self.interest_rate
+        return self._balance
+    
+    def withdraw(self, amount):
+        print("Withdrawals are not allowed from Children's account")
+        return self._balance
+
