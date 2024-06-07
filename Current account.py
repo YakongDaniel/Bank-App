41a5b9291bb288d69cb135bbf044c22837fa875c
samarimaryYP <samari.mary@binghamuni.edu.ng>
2024-06-07 13:02:24 +0100

    Current account.py

--- Current account.py
+++ Current account.py
@@ -1,5 +1,5 @@
   #Current account with no restrictions
 
 class CurrentAccount(Account):
-    def __init__(self, account_number, holder_name, balance=0.0):
-        super().__init__(account_number, holder_name, balance)
+    def __init__(self, account_number, your_name, balance=0.0):
+        super().__init__(account_number, your_name, balance)
